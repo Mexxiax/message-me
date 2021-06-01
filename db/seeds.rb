e86f42ba0b1f7be43d6cf340b5722f408f6c1524
messias <miguelsoares91@gmail.com>
2021-06-01 21:36:36 +0100

    user and message resources and hirb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+User.create(username: "User1", email: "user1@gmail.com", admin: false, password: "password")
+User.create(username: "User2", email: "user2@gmail.com", admin: false, password: "password")
+User.create(username: "User3", email: "user3@gmail.com", admin: false, password: "password")
+User.create(username: "User4", email: "user4@gmail.com", admin: true, password: "password")
+User.create(username: "User5", email: "user5@gmail.com", admin: false, password: "password")
+User.create(username: "User6", email: "user6@gmail.com", admin: false, password: "password")
+User.create(username: "User7", email: "user7@gmail.com", admin: false, password: "password")
+User.create(username: "User8", email: "user8@gmail.com", admin: false, password: "password")
+User.create(username: "User9", email: "user9@gmail.com", admin: false, password: "password")
+User.create(username: "User10", email: "user10@gmail.com", admin: false, password: "password")
+User.create(username: "User11", email: "user11@gmail.com", admin: false, password: "password")
